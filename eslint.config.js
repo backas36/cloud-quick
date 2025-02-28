244775286a75b8b022a6f9b4f95712f0c5b10b62
Huiyuan Yang <77166030+backas36@users.noreply.github.com>
2025-03-01 00:02:28 +0800

    setup prettier, eslint airbnb

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,80 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+
+import airbnb from "eslint-config-airbnb";
+import airbnbTs from "eslint-config-airbnb-typescript";
+import prettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import pluginPrettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ["dist"] },
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+            react,
+            "jsx-a11y": jsxA11y,
+            import: importPlugin,
+            prettier: pluginPrettier,
+        },
+        rules: {
+            "import/extensions": [
+                "error",
+                "ignorePackages",
+                {
+                    js: "never",
+                    jsx: "never",
+                    ts: "never",
+                    tsx: "never",
+                },
+            ],
+            "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+            "react/require-default-props": "off",
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "prettier/prettier": [
+                "error",
+                {
+                    singleQuote: false,
+                    tabWidth: 4,
+                    semi: true,
+                    useTabs: false,
+                    jsxSingleQuote: true,
+                    trailingComma: "es5",
+                    bracketSpacing: true,
+                    arrowParens: "always",
+                    printWidth: 120,
+                },
+            ],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [...tseslint.configs.recommended],
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+        },
+    }
+);
